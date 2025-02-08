757ff5570d58b12ef015e5bef03b895cc47b7f82
akbar-demandesk <adops@demandesk.com>
2025-02-08 20:44:26 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,9 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === "development",
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default withPwa({
