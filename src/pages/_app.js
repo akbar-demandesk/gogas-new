08a345ee918f04c715142eec400ee32e23234ba4
akbar-demandesk <adops@demandesk.com>
2025-02-09 17:39:20 +0530

    chng

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,7 @@ export default function App({ Component, pageProps }) {
   useEffect(() => {
     if ("serviceWorker" in navigator) {
       navigator.serviceWorker
-        .register("/servicer-worker/sw-new.js", { scope: "/" }) // 🔥 Ensure the correct scope
+        .register("/servicer-worker/sw-new.js", { scope: "/servicer-worker/" }) // ✅ Adjusted scope
         .then((reg) => console.log("✅ Service Worker registered", reg))
         .catch((error) =>
           console.error("❌ Service Worker registration failed:", error)
